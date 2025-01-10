b0fb5c10c065088b06161c278a6bacc19f11e368
grandmasterkd <kamoah@redpearcommunications.com>
2025-01-10 11:25:46 +0000

    update: resto media logo

--- pages/service/rmedia.tsx
+++ pages/service/rmedia.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import luxevent from "@/public/mediahero.jpg";
 import Image from "next/image";
+import mlogo from "@/public/rmedialogo.svg";
 
 function RMedia() {
   return (
     <main className="">
       <section className="w-full min-h-screen flex items-center justify-center">
-        <div className="w-full h-full object-cover absolute top-0 left-0">
+        <div className="w-full h-full object-cover absolute top-0 left-0 z-0">
           <Image
             src={luxevent}
             alt="luxevent"
@@ -14,6 +15,15 @@ function RMedia() {
             objectFit="cover"
             className="w-full h-full"
           />
+          <span className="min-h-screen relative z-10 flex items-center justify-center">
+            <Image
+              src={mlogo}
+              alt="restomedialogo"
+              width={100}
+              height={100}
+              className=""
+            />
+          </span>
         </div>
       </section>
 
